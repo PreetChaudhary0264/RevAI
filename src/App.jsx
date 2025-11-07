ab88cc98dfb49de613785c55f47d8ac7dd1c3533
Preet Kaler <preetkalerop@gmail.com>
2025-11-07 23:19:13 +0530

    wake

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Index from "./pages/Index";
 import GetStarted from "./pages/GetStarted";
 import NotFound from "./pages/NotFound";
 import { ToastContainer } from 'react-toastify';
+import Warmup from './pages/Warmup';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/get-started" element={<GetStarted />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/wake" element={<Warmup />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer/>
